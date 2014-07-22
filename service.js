3e95525452ccd971ab79677f6a4751c897ae9e47
Theo Julienne <theo.julienne@gmail.com>
2014-07-22 11:00:48 +1000

    Initial implementation.

---
+++ service.js
@@ -0,0 +1,17 @@
+process.title = 'sphere-cloud-modelstore';
+
+if (process.env.BUGSNAG_KEY) {
+  var bugsnag = require("bugsnag");
+  bugsnag.register(process.env.BUGSNAG_KEY, { releaseStage: process.env.USVC_CONFIG_ENV || 'development' });
+}
+
+var usvc = require('usvc');
+
+var service = usvc.microService({
+  // backing stores
+  couchdb: usvc.facets.db.couchdb(),
+
+  // rpc interface
+  rpcService: usvc.facets.rpc.jsonServer(['modelStoreService']),
+  modelStoreService: require('./lib/rpc'),
+}).run();
